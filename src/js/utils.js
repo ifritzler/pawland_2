9939db4f4edac3dbe560056f437688cd325717c4
Ilan Emanuel Fritzler <contacto.fritzlerilan@gmail.com>
2022-08-01 23:06:35 -0300

    toastifying my notifications

---
+++ src/js/utils.js
@@ -0,0 +1,21 @@
+const showToastNotification = ({ title }, callback = null) => {
+  Toastify({
+    text: `${title} fue agregado!`,
+    duration: 3000,
+    newWindow: false,
+    close: true,
+    gravity: "top", // `top` or `bottom`
+    position: "right", // `left`, `center` or `right`
+    stopOnFocus: true, // Prevents dismissing of toast on hover
+    style: {
+      background: "rgba(255, 208, 0, 1)",
+      color: "rgba(24, 24, 24, 1)",
+      fontSize: "16px"
+    },
+    onClick: callback ? callback : null, // Callback after click
+  }).showToast();
+};
+
+export {
+  showToastNotification
+}
--- src/js/views/store.js
+++ src/js/views/store.js
@@ -2,30 +2,33 @@ import card from "../components/card.js";
 
 import { getAllProducts, getProductById } from "../services/products.js";
 import { pushToCart } from "../services/addItem.js";
+import { showToastNotification } from "../utils.js";
+
+const saveProductToCart = async (event) => {
+  event.preventDefault();
+  const { id } = event.target.dataset;
+  const product = await getProductById(id);
+  pushToCart(product);
+  showToastNotification(product, () => console.log("hello from toast"));
+};
 
 // Componente que se encarga de renderizar las cards dentro de si y se devuelve para ser pintado
-const cards = async () => {
+const cards = async (filters = null) => {
   // Separacion de responsabilidad. El fetching lo realiza el servicio de getAllProducts()
   const data = await getAllProducts();
   const cardsContainer = document.createElement("ul");
   cardsContainer.classList.add("cards");
 
   let tarjetas = "";
-  data.forEach(product => tarjetas += card(product));
+  data.forEach((product) => (tarjetas += card(product)));
   cardsContainer.innerHTML = tarjetas;
 
-  const btnsAgregar = cardsContainer.getElementsByClassName('btn-agregar')
-  if(btnsAgregar){
-    for(let i = 0; i < btnsAgregar.length; i++){
-      btnsAgregar[i].addEventListener('click', async (e) => {
-        e.preventDefault()
-        const id = btnsAgregar[i].dataset.id
-        console.log(id)
-        const product = await getProductById(id)
-        pushToCart(product)
-      })
-    }
-  }
+  const btnsAgregar = cardsContainer.getElementsByClassName("btn-agregar");
+
+  Array.from(btnsAgregar).forEach((btn) => {
+    btn.addEventListener("click", saveProductToCart);
+  });
+
   return cardsContainer;
 };
 
